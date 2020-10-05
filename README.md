0179771fe92297ecee45f8f5cac55ee40e75c003
adafia <adafia.samuel@gmail.com>
2020-10-05 10:26:16 +0200

    deploy to firebase hosting

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## Sneacker City (client)
+Client side of [Sneaker-city](https://github.com/adafia/sneaker-city) API (backend)
+
+### Hosting
+This app is hosted on Google's Firebase. You can access it via this [link](https://sneaker-city-9f597.web.app/) 
--- src/core/fixedPrices.js
+++ src/core/fixedPrices.js
@@ -6,22 +6,27 @@ export const prices = [
   },
   {
     _id: 1,
-    name: '$0 to $9',
-    array: [0, 9]   
-  },
-  {
-    _id: 3,
     name: '$10 to $19',
     array: [10, 19]   
   },
   {
-    _id: 4,
+    _id: 3,
     name: '$20 to $29',
     array: [20, 29]   
   },
   {
+    _id: 4,
+    name: '$30 to $39',
+    array: [30, 39]   
+  },
+  {
     _id: 5,
-    name: 'More than $40',
-    array: [40, 99]   
+    name: '$40 to $49',
+    array: [40, 49]   
+  },
+  {
+    _id: 6,
+    name: 'More than $50',
+    array: [50, 99]   
   }
 ]
